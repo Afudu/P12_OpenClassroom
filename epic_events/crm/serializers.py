2c50e29c1c38e664c14bd711b8e1325d9451a5a6
Afudu <abdoulseydou@hotmail.com>
2024-11-24 22:24:43 +0000

    Create serializers.py

---
+++ epic_events/crm/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from crm.models import User, Client, Contract, Event
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  'password',
+                  'role',
+                  'is_active',
+                  'is_admin',)
+
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+class ClientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Client
+        fields = ["id",
+                  "first_name",
+                  "last_name",
+                  "email",
+                  "phone",
+                  "mobile",
+                  "company_name",
+                  "date_created",
+                  "date_updated",
+                  "sales_contact",
+                  "status"
+                  ]
+
+
+class ContractSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Contract
+        fields = ["id",
+                  "client",
+                  "date_created",
+                  "date_signed",
+                  "date_updated",
+                  "amount",
+                  "status"
+                  ]
+
+
+class EventSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Event
+        fields = ["id",
+                  "name",
+                  "contract",
+                  "start_date",
+                  "end_date",
+                  "support_contact",
+                  "notes",
+                  "status"
+                  ]
