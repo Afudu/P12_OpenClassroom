6533534dda821264d8c6f5f51632296205f07259
Afudu <abdoulseydou@hotmail.com>
2024-11-30 13:23:18 +0000

    Update permissions.py

--- epic_events/crm/permissions.py
+++ epic_events/crm/permissions.py
@@ -11,7 +11,6 @@ class IsManagement(BasePermission):
 
 
 class IsClientSalesContactOrReadOnly(BasePermission):
-
     """
     Permissions:
     - Sales: Can create and manage clients they are assigned to.
@@ -68,3 +67,19 @@ class IsEventSalesOrSupportContact(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return obj.contract.client.sales_contact == request.user or obj.support_contact == request.user
+
+
+class UserPermission(BasePermission):
+    """
+      Object-level permission to only allow users to update their own account.
+    """
+    message = "You can only access and update your own account."
+
+    def has_object_permission(self, request, view, obj):
+
+        if view.action in ["retrieve", "update", "partial_update"]:
+            return (
+                    obj == request.user
+            )  # Allow the user to retrieve, update or partial_update their own data
+        else:
+            return False  # For other actions, deny all requests
